72b76627475f1bc56f915cde6043e9f01a68e0c2
jessicacosta32 <jessicostaolivera@gmail.com>
2023-08-16 19:20:07 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,11 +1,38 @@
-document.addEventListener("DOMContentLoaded", ()=>{
+let estaLogeado = false;
 
-    let loginform = document.getElementById("login-form");
-    let usernameInput = document.getElementById("username-input");
+const btnSignIn = document.getElementById("sign-in");
+const btnSignUp = document.getElementById("sign-up");
+const formRegister = document.querySelector(".register");
+const formLogin = document.querySelector(".login");
+
+const loginForm = document.getElementById("loginForm");
+
+btnSignUp.addEventListener("click", e => {
     
-    loginform.addEventListener("submit", (e)=>{
-        e.preventDefault();
-        sessionStorage.setItem("username", usernameInput.value);
-        window.location = "index.html";
-    })
-})
+
+    formLogin.classList.add("hide");
+    formRegister.classList.remove("hide")
+});
+
+btnSignIn.addEventListener("click", e => {
+    
+    formRegister.classList.add("hide");
+    formLogin.classList.remove("hide")
+});
+
+
+loginForm.addEventListener("submit", e => {
+    const user = document.getElementById("usuario").value;
+    
+
+    if(user !== ""){
+        estaLogeado = true;
+    if(estaLogeado){
+        sessionStorage.setItem("estado", estaLogeado);
+        window.location.href = "./index.html";
+    }
+    }else{
+        alert("Completa el Usuario");
+    }
+    
+});
