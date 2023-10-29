bed2f6b5dee188d5653d27b753266832faf5df5c
Santiago Giraudin <gira1849@gmail.com>
2023-10-28 22:41:42 -0300

    commit final entrega 6

--- js/cart.js
+++ js/cart.js
@@ -3,6 +3,8 @@ let productoAPIeliminado = false;
 document.addEventListener('DOMContentLoaded', async function () {
     await showProducts();
     await metodoDePago();
+    
+    cargarSubTotal();
 });
 
 async function showProducts()
@@ -48,18 +50,20 @@ async function showProducts()
             container.appendChild(linea(localCart[i]));
         }
     }
-
+    
     // Agregar evento a cada input con la clase "cantidadDeProducto"
     const inputDeCantidad = document.getElementsByClassName("cantidadDeProducto");
     for (let i = 0; i < inputDeCantidad.length; i++) {
         const idInput = inputDeCantidad[i].id;
         inputDeCantidad[i].addEventListener("input", () => { cambioDePrecio(idInput, localCart.find((p) => p.id == idInput)) });
+        inputDeCantidad[i].addEventListener("input", () => { cargarSubTotal() });
     }
 }
 
 function linea(product) {
     // se crea el botón de eliminar
-    let deleteButton = document.createElement("div");
+    let deleteButton = document.createElement("td");
+    deleteButton.classList.add("ms-3");
     deleteButton.innerHTML = `<button class="btn btn-outline-danger"><i class="fa-solid fa-trash" style="color: #da3434;"></i></button>`;
     // se le agrega el evento de click
     deleteButton.addEventListener("click", ev=>{
@@ -79,10 +83,10 @@ function linea(product) {
             // se vuelven a cargar los elementos.
             showProducts();
         }else{
-        // de lo contrario, si el elemento tiene la id 50924, significa que es el producto cargado desde la API.
-        // este producto no se encuentra guardado en el localStorage por lo que se debe eliminar de forma diferente a los demás productos.
-        // se elimina quitando el primer elemento hijo del contenedor. (se asume que el producto 50924 siempre estará en el primer lugar)
-
+            // de lo contrario, si el elemento tiene la id 50924, significa que es el producto cargado desde la API.
+            // este producto no se encuentra guardado en el localStorage por lo que se debe eliminar de forma diferente a los demás productos.
+            // se elimina quitando el primer elemento hijo del contenedor. (se asume que el producto 50924 siempre estará en el primer lugar)
+            
             productoAPIeliminado = true;
             // se busca el contenedor HTML de los productos del carrito.
             const container = document.getElementById("cart-product-container");
@@ -91,17 +95,20 @@ function linea(product) {
                 container.removeChild(container.firstChild);
             }
         }
+        // actualiza el costo final
+        cargarSubTotal();
     });
     
     // elemento final del producto que se agregará al contenedor
     let resultElement = document.createElement("tr");
+    resultElement.setAttribute("valign", "middle");
     resultElement.innerHTML = 
     `
     <td class="w-25"><img src="${product.images[0]}" class="w-75"></td>
     <td class="w-25">${product.name}</td>
     <td class="w-25">${product.currency} ${product.cost}</td>
-    <td><input value="1" id="${product.id}" type="number"  min="1" class="cantidadDeProducto"></td>
-    <td class="w-25" id="precioPorCantidad${product.id}" >${product.currency} ${product.cost}</td>
+    <td><input value="1" id="${product.id}" type="number"  min="1" class="cantidadDeProducto form-control"></td>
+    <td class="w-25 text-end" id="precioPorCantidad${product.id}" >${product.currency} ${product.cost}</td>
     `;
     
     // luego al elemento final se le agrega el botón
@@ -133,22 +140,158 @@ async function metodoDePago () {
     const codSegu = document.getElementById('codSegu');
     const vencimiento = document.getElementById('vencimiento');
     const nroCuenta = document.getElementById('nroCuenta');
-
+    
     tarjetaDeCredito.addEventListener('change', () => {
-      nroTarjeta.disabled = false;
-      codSegu.disabled = false;
-      vencimiento.disabled = false;
-      nroCuenta.disabled = true;
-      divShow.innerHTML = ``;
-      divShow.innerHTML += `Tarjetas de crédito`;
+        nroTarjeta.disabled = false;
+        codSegu.disabled = false;
+        vencimiento.disabled = false;
+        nroCuenta.disabled = true;
+        divShow.innerHTML = ``;
+        divShow.innerHTML += `Tarjetas de crédito`;
     });
-  
+    
     transferenciaBancaria.addEventListener('change', () => {
-      nroTarjeta.disabled = true;
-      codSegu.disabled = true;
-      vencimiento.disabled = true;
-      nroCuenta.disabled = false;
-      divShow.innerHTML = ``;
-      divShow.innerHTML += `Transferencia bancaria`;
+        nroTarjeta.disabled = true;
+        codSegu.disabled = true;
+        vencimiento.disabled = true;
+        nroCuenta.disabled = false;
+        divShow.innerHTML = ``;
+        divShow.innerHTML += `Transferencia bancaria`;
     });
 }
+
+// Obtener todos los radio buttons con name "tipoEnvio"
+const envioRadios = document.querySelectorAll('input[name="tipoEnvio"]');
+// Agregar un event listener a cada radio button
+envioRadios.forEach((radio) => {
+    radio.addEventListener("change", calcularCostoEnvio);
+});
+
+function cargarSubTotal(){
+    const container = document.getElementById("cart-product-container");
+    let subTotal = 0;
+    
+    if (container) {
+        const filas = container.querySelectorAll('tr');
+        
+        filas.forEach(fila => {
+            const columnas = fila.querySelectorAll('td'); 
+            const valorColumna = columnas[4];
+            const subTot = valorColumna.textContent;
+            const precioFinalSubtotal = subTot.split(" ");
+            subTotal += parseInt(precioFinalSubtotal[1]);
+        });
+        
+        const contenedorSubTotalGeneral = document.getElementById("subtotalGeneral");
+        contenedorSubTotalGeneral.innerHTML = `$${subTotal}` ;
+        
+    }
+    
+    calcularCostoEnvio();
+}
+function calcularCostoEnvio() {
+    const subTotal = parseFloat(document.getElementById("subtotalGeneral").textContent.replace('$', ''));
+    let costoEnvio = 0;
+    envioRadios.forEach((radio) => {
+        if (radio.checked) {
+            const porcentaje = parseFloat(radio.value); // Obtén el porcentaje del valor del radio
+            costoEnvio = (subTotal * porcentaje) / 100; // Calcula el costo de envío
+        }
+    });
+    
+    const contenedorCostoEnvio = document.getElementById("costoEnvio");
+    contenedorCostoEnvio.innerHTML = `$${costoEnvio.toFixed(2)}`;
+    actualizarTotal(subTotal, costoEnvio)
+    
+}
+function actualizarTotal(subTotal, costoEnvio) {
+    const total = subTotal + costoEnvio;
+    const contenedorTotal = document.getElementById("total");
+    contenedorTotal.innerHTML = `$${total.toFixed(2)}`;
+}
+
+// Validaciones
+
+document.getElementById("finalizarCompra").addEventListener('click', function(event) {
+    event.preventDefault(); // Evita el envío del formulario
+  
+      let todoCorrecto = true; // Asumimos que todo está correcto al inicio
+  
+      // 1. Validación dirección
+      const calle = document.getElementById('calle').value;
+      const numero = document.getElementById('numero').value;
+      const esquina = document.getElementById('esquina').value;
+      
+      if (!calle) {
+        document.getElementById('feedbackCalle').innerText = 'Ingresar una calle';
+        todoCorrecto = false; // Hay un error
+      } else {
+        document.getElementById('feedbackCalle').innerText = '';
+      }
+  
+      if (!numero) {
+        document.getElementById('feedbackNumero').innerText = 'Ingresar un número';
+        todoCorrecto = false; // Hay un error
+      } else {
+        document.getElementById('feedbackNumero').innerText = '';
+      }
+  
+      if (!esquina) {
+        document.getElementById('feedbackEsquina').innerText = 'Ingresar una esquina';
+        todoCorrecto = false; // Hay un error
+      } else {
+        document.getElementById('feedbackEsquina').innerText = '';
+      }
+  
+      // 2. Validación tipo de envío
+      const tipoEnvio = document.querySelectorAll('input[name="tipoEnvio"]:checked');
+      if (tipoEnvio.length === 0) {
+        document.getElementById('feedbackEnvio').innerText = 'Debe seleccionar un tipo de envío.';
+        todoCorrecto = false; // Hay un error
+      } else {
+        document.getElementById('feedbackEnvio').innerText = '';
+      }
+  
+      // 3. Validación forma de pago
+      const formaPago = document.querySelectorAll('input[name="pago"]:checked');
+      if (formaPago.length === 0) {
+        document.getElementById('feedbackPago').innerText = 'Debe seleccionar una forma de pago.';
+        todoCorrecto = false; // Hay un error
+      } else {
+        if (formaPago[0].id === 'tarjetaDeCredito') {
+          // Validación tarjeta de crédito
+          const nroTarjeta = document.getElementById('nroTarjeta').value;
+          const codSegu = document.getElementById('codSegu').value;
+          const vencimiento = document.getElementById('vencimiento').value;
+          if (!nroTarjeta || !codSegu || !vencimiento) {
+            document.getElementById('feedbackPago').innerText = 'Todos los campos de tarjeta de crédito son obligatorios.';
+            todoCorrecto = false; // Hay un error
+          } else {
+            document.getElementById('feedbackPago').innerText = '';
+          }
+        } else if (formaPago[0].id === 'transferenciaBancaria') {
+          // Validación transferencia bancaria
+          const nroCuenta = document.getElementById('nroCuenta').value;
+          if (!nroCuenta) {
+            document.getElementById('feedbackPago').innerText = 'El número de cuenta es obligatorio para transferencia bancaria.';
+            todoCorrecto = false; // Hay un error
+          } else {
+            document.getElementById('feedbackPago').innerText = '';
+          }
+        }
+      }
+  
+      // Mostrar mensaje de éxito si todo está correcto
+      if (todoCorrecto) {
+        const notification = document.getElementById("notification");
+        const notificationText = document.getElementById("notificationText");
+        
+        notificationText.innerText = '¡Has comprado con éxito!';
+        notification.classList.add("show");
+  
+        // Ocultar la notificación después de un tiempo
+        setTimeout(function() {
+          notification.classList.remove("show");
+      }, 3000); // Ocultar después de 3 segundos
+      }
+  });
